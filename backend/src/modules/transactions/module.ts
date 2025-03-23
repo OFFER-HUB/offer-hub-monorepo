91b7acee2e0010f62c90a7f90140a7e3b60a6fb2
jimenezz22 <ljimenezbogantes@gmail.com>
2025-03-23 14:53:39 -0600

    feat: update NestJS dependencies to version 11.0.12 and integrate InvoiceModule into TransactionsModule

--- backend/src/modules/transactions/module.ts
+++ backend/src/modules/transactions/module.ts
@@ -5,9 +5,13 @@ import { TransactionsService } from "./service";
 import { Transaction } from "./entity";
 import { User } from "../users/entity";
 import { Project } from "../projects/entity";
+import { InvoiceModule } from "../invoices/module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, User, Project])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction, User, Project]),
+    InvoiceModule
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
   exports: [TransactionsService],
